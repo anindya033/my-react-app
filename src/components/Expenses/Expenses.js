ca751e05454650cf2fbd3121277b179dd565ed02
anindya033 <anindya033paul@gmail.com>
2022-04-15 12:37:46 +0530

    Add filter component. Set default value of a dropdwon from parent component; using STATE.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import './Expenses.css';
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+    
+    const [filteredYear,setFilteredYear] = useState("2020");
+    const onFilterChangeHandler = (data)=>{
+        console.log("From Expense .js");
+        console.log(data);
+        setFilteredYear(data);
+    }
   return (
     <Card className="expenses">
+      <ExpensesFilter 
+      defaultYearValue={filteredYear}
+      onFilterChangeData={onFilterChangeHandler} />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -29,6 +40,6 @@ const Expenses = (props) => {
       />
     </Card>
   );
-}
+};
 
 export default Expenses;
