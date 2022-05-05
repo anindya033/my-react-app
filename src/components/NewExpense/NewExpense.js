8abea3777870f1bacc95345cc92e0e060a9cdc9e
anindya033 <anindya033paul@gmail.com>
2022-05-05 15:44:27 +0530

    Conditional show/hide add new expense button

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState } from 'react';
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
-
     const expenseData = {
-        ...enteredExpenseData,
-        id : Math.random().toString()
+      ...enteredExpenseData,
+      id: Math.random().toString(),
     };
     console.log("Data coming form child expenseData");
     console.log(expenseData);
@@ -16,9 +16,24 @@ const NewExpense = (props) => {
     //passing data : Child to Parent ; to app.js
     props.onAddExpense(expenseData);
   };
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
