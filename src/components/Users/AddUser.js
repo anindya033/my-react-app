8338910c1e9bfb4f192fab1401b85d9f3b3c4daf
anindya033 <anindya033paul@gmail.com>
2022-07-03 02:40:35 +0530

    Base designing using props

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,17 +1,20 @@
 import React from "react";
+import Card from "../UI/Card";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">User Name</label>
-      <input type={Text} id="username" />
-      <label htmlFor="age">age</label>
-      <input type={Number} id="age" />
-      <input type={"submit"} value="Add User" />
-    </form>
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">User Name</label>
+        <input type={Text} id="username" />
+        <label htmlFor="age">age</label>
+        <input type={Number} id="age" />
+        <input type={"submit"} value="Add User" />
+      </form>
+    </Card>
   );
 };
 
