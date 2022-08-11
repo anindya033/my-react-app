bb519980797a0dd4175856f7d10916416e6e5683
anindya033 <anindya033paul@gmail.com>
2022-08-12 03:47:26 +0530

    Moves Items to MealItem component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../UI/Card";
 import classes from './AvailableMeals.module.css'
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
     {
@@ -30,31 +31,30 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+    const mealList = DUMMY_MEALS.map(meal =>
+        <MealItem
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        >
+
+        </MealItem>
+    );
     console.log("Meals");
     console.log(mealList);
     return (
 
-        // <section className={classes.meals}>
-        //     <ul>
-        //         {
-        //             mealList
-        //         }
-        //     </ul>
-        // </section>
+        <section className={classes.meals}>
+            <Card>
+                <ul>
+                    {
+                        mealList
+                    }
+                </ul>
+            </Card>
+        </section>
 
-
-        <React.Fragment>
-            {
-                DUMMY_MEALS.map((meal) => (
-                    <Card>
-                        <li>
-                            {meal.name}
-                        </li>
-                    </Card>
-                ))
-            }
-        </React.Fragment>
     )
 };
 
