25b6d4d3af24ab8ad481fb88a3afec74d6af3bec
anindya033 <anindya033paul@gmail.com>
2023-02-11 18:52:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
-import React from 'react';
-
-import MoviesList from './components/MoviesList';
-import './App.css';
+import SimpleInput from './components/SimpleInput';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
-
   return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
+    <div className="app">
+      <SimpleInput />
+    </div>
   );
 }
 
